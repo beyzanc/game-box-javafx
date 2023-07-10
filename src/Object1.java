463cf12f269e7852e3cd6ad5f4073d0f74591d57
Beyza Çabuk <beyzancabuk@gmail.com>
2023-07-10 21:06:24 +0300

    Create Object1.java

---
+++ src/Object1.java
@@ -0,0 +1,14 @@
+package gamebox;
+
+import java.util.ArrayList;
+
+public class Object1 {
+    int score;
+    ArrayList<String> locations;
+
+    public Object() {
+        this.locations = new ArrayList<String>();
+        this.score = 0;
+    }
+
+}
